b725198f31d9e1cf219af00c3abf5be7274d4cdd
Jagdeep Kaur <34484773+jagdeep1001@users.noreply.github.com>
2026-04-21 17:32:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
+const repo = "b_6yKyYCXlTPs"
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 }
+
 
 export default nextConfig
